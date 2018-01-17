50038cdf32aea7f5ef914528fe514322c84999f7
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-01-17 14:28:46 -0800

    add and find methods complete

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ mongoose.promise = global.Promise;
 const db = mongoose.connect('mongodb://localhost:27017/customerinfo', {
   useMongoClient: true
 });
+
+//import of model
+const Customer = require('./models/customer');
+
+//Add Customer
+const addCustomer = (customer) => {
+  Customer.create(customer).then(customer => {
+    console.info('New Customer Added');
+    db.close();
+  });
+}
+
+//Find customer
+const findCustomer = (name) => {
+  //Case insensitive
+  const search = new RegExp(name, 'i');
+  Customer.find({$or: [{firstname: search}. {lastname: search}]})
+    .then(customer => {
+      console.info(customer);
+      console.info(`${customer.length} matches`);
+      db.close();
+    });
+}
+
+//Export Methods
+module.exports = {
+  addCustomer,
+  findCustomer
+}

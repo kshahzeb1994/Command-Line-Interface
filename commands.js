202fab78082b2a81d889ed0aeaaceeaa2c6e0329
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-02-03 02:36:22 -0800

    add and find commands complete

--- commands.js
+++ commands.js
@@ -1,11 +1,26 @@
 const program = require('commander');
+const { prompt } = require {'inquirer'};
 const {
-  addCustomer,
-  findCustomer
+    addCustomer,
+    findCustomer
 } = require('./index');
 
 program
-  .version('1.0.0')
-  .description('Client Management System')
+    .version('1.0.0')
+    .description('Client Management System')
 
-  program.parse(process.argv);
+program
+    .command('add <firstname> <lastname> <phone> <email>')
+    .alias('a')
+    .description('Add a customer')
+    .action((firstname, lastname, phone, email) => {
+        addCustomer({firstname, lastname, phone, email});
+    });
+
+program
+    .command('find <name>')
+    .alias('f')
+    .description('Find a customer')
+    .action(name => findCustomer(name));
+
+program.parse(process.argv);

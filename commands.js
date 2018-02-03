446b249e5ceb283c8b99d7ac487193964f128f69
kshahzeb1994 <kshahzeb1994@gmail.com>
2018-02-03 02:52:12 -0800

    added inquirer for add and find

--- commands.js
+++ commands.js
@@ -1,20 +1,47 @@
 const program = require('commander');
-const { prompt } = require {'inquirer'};
+const { prompt } = require ('inquirer');
 const {
     addCustomer,
     findCustomer
 } = require('./index');
 
+const question = [
+{    type: 'input',
+    name: 'firstname',
+    message: 'customer\'s first name'
+},
+{    type: 'input',
+    name: 'lastname',
+    message: 'customer\'s last name'
+},
+{    type: 'input',
+    name: 'phone',
+    message: 'customer\'s phone number'
+},
+{    type: 'input',
+    name: 'email',
+    message: 'customer\'s email address'
+}
+];
+
 program
     .version('1.0.0')
     .description('Client Management System')
 
+// program
+//     .command('add <firstname> <lastname> <phone> <email>')
+//     .alias('a')
+//     .description('Add a customer')
+//     .action((firstname, lastname, phone, email) => {
+//         addCustomer({firstname, lastname, phone, email});
+//     });
+
 program
-    .command('add <firstname> <lastname> <phone> <email>')
+    .command('add')
     .alias('a')
     .description('Add a customer')
-    .action((firstname, lastname, phone, email) => {
-        addCustomer({firstname, lastname, phone, email});
+    .action(() => {
+        prompt(question).then(answers => addCustomer(answers))
     });
 
 program
